718ef3eceb68cb2842236a17d435977bb0cb7c0d
madisonlutz <mrl440@nyu.edu>
2017-02-26 21:59:06 -0500

    Added a dot that the targets never quite hit

--- sketch.js
+++ sketch.js
@@ -13,14 +13,14 @@ function setup() {
 function draw() { 
 
   background (second()*4.3, second()*4.3, second()*4.3);
-  
-
   target ((second()*(random(10))), 300);
   target (300, (second()*(random(10))));
   target (0, (second()*(random(10))));
   target ((second()*(random(10))), 0);
   target (600, (second()*(random(10))));
   target ((second()*(random(10))), 600);
+  
+  dot (300, 300);
 }
 
 function target(x, y){
@@ -32,11 +32,15 @@ function target(x, y){
   	ellipse (x, y, 65, 65);
   	fill (211, 0, 0);
   	ellipse (x, y, 30, 30);
+} // target end
   
 function crosshair (x, y){
 //I am trying to get one of those little crosshair + signs to follow my
 //cursor but I forget how to do that...
   
-}
+} //crosshair end
   
-}// target end
+function dot (x, y){
+  fill(255 - (second()*4.25));
+  ellipse ((second()*4)+x, (second()*4)+y, 10, 10);
+}
