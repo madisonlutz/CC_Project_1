901713a5c1d537fa79d41575f7a596d13f9746f5
madisonlutz <mrl440@nyu.edu>
2017-03-05 17:06:54 -0500

    Added lasers plus an easy mode and a standard mode

--- sketch.js
+++ sketch.js
@@ -1,27 +1,99 @@
-/* Was getting stressed and frustrated working on this project. I 
-suppose this is a manifestation of those adjectives. But I kind of like
-where this could go!
-*/
 
-
+var sMeter;
+var lasRandX;
+var lasRandY;
+var tarRandX;
+var tarRandY;
+var value = 1;
 
 function setup() { 
-  createCanvas(600, 600);
-  frameRate(hour());
+  cursor(CROSS);
+  createCanvas(600, 400);
+  ellipseMode(CENTER);
+  stroke(0);
+
 } 
 
 function draw() { 
 
-  background (second()*4.3, second()*4.3, second()*4.3);
-  target ((second()*(random(10))), 300);
-  target (300, (second()*(random(10))));
-  target (0, (second()*(random(10))));
-  target ((second()*(random(10))), 0);
-  target (600, (second()*(random(10))));
-  target ((second()*(random(10))), 600);
+	background(0);
   
-  dot (300, 300);
+//target focus circle
+  strokeWeight(1);
+	noFill();
+  stroke('red');
+//circle shooter aiming thing
+  ellipse(mouseX,mouseY, 17, 17);
+
+//return to start
+	if (second()==0 || second()==1 || second()==2) {
+    start(100,200,40);
+    tarRandX = 0;
+    tarRandY = 0;
+  }//if end
+  
+//targets begin random movement
+  else {
+    tarRandX = random(50,550);
+  	tarRandY = random(50,350);
+  } //else end
+  
+//if the laser hits a randomly moving target display 'HIT!' 
+  if (tarRandX == lasRandX && tarRandY == lasRandY && mouseIsPressed && value == 1){
+    hit(533, 30, 20);
+  }//if end
+  
+//if the laser misses a randomly moving target display 'MISS!'
+  if (tarRandX != lasRandX && tarRandY != lasRandY && mouseIsPressed && value == 1){
+    miss(533, 30, 20);
+  }//if end
+  
+//easy mode hit 
+  if (value == 0 && mouseIsPressed){
+    hit(543, 30, 20);
+  }//if end
+  
+//moving target if value == 1
+  if (value == 1) {
+  	target (tarRandX, tarRandY);
+    standard(27, 30, 20);
+  }
+  
+//moving target if value == 0
+  if (value == 0) {
+  	target (lasRandX, lasRandY);
+    easy(27,30,20);
+  }
+  
+//countdown meter
+  meter();
+  
+//green lasers
+  laser();
+  
 }
+
+function meter(){
+  sMeter = second()*5;
+  fill('red');
+  noStroke();
+  rect(550, 50+sMeter, 25, 300-sMeter);
+  noFill();
+  strokeWeight(5);
+  stroke(255);
+  rect(550, 50, 25, 300);
+  
+}
+
+function laser(){
+  if (mouseIsPressed) {
+  	lasRandX = random(50,550);
+  	lasRandY = random(50, 350);
+  	print("testing");
+  	stroke(0, 255, 4);
+  	line(mouseX, mouseY, lasRandX, lasRandY);
+  }
+}	
 
 function target(x, y){
   	ellipseMode(CENTER);
@@ -33,14 +105,52 @@ function target(x, y){
   	fill (211, 0, 0);
   	ellipse (x, y, 30, 30);
 } // target end
-  
-function crosshair (x, y){
-//I am trying to get one of those little crosshair + signs to follow my
-//cursor but I forget how to do that...
-  
-} //crosshair end
-  
-function dot (x, y){
-  fill(255 - (second()*4.25));
-  ellipse ((second()*4)+x, (second()*4)+y, 10, 10);
-}
+
+function start(x, y, s){
+  fill(255);
+  noStroke();
+  textSize(s);
+  text("RETURN TO START", x, y); 
+} //start end
+
+function hit(x, y, s){
+  fill('green');
+  noStroke();
+  textSize(s);
+  text("HIT!", x, y);
+} //hit end
+
+function miss(x, y, s){
+  fill('red');
+  noStroke();
+  textSize(s);
+  text("MISS!", x, y);
+} //miss end
+
+function keyPressed() {
+  if (value === 0) {
+    value = 1;
+    print('1');
+  } else {
+    value = 0;
+    print('0');
+  }
+} //keyPressed end
+
+function standard(x, y, s){
+  fill(255);
+  noStroke();
+  textSize(s);
+  text("standard mode", x, y);
+} //miss end
+
+function easy(x, y, s){
+  fill(255);
+  noStroke();
+  textSize(s);
+  text("easy mode", x, y);
+} //miss end
+
+
+
+
